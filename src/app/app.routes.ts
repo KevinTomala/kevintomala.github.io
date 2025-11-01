979723203c01369349563d9f51a3059e5889756d
Kevin Tomala <kevintomala.27@gmail.com>
2025-11-01 00:18:00 -0500

    Update AboutComponent content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
-
+import { AboutComponent } from './about/about.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'portfolio', component: PortfolioComponent }, // 👈 Nueva ruta
   { path: 'contact', component: ContactComponent },     // 👈 Nueva ruta
+  { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: '' },
 ];
